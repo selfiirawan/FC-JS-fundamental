5b290423fc7bca85555c8b14a56a519fd8e5452e
selfiirawan <celpi.irawan@gmail.com>
2026-08-17 09:32:13 +0800

    modify first exercise

--- index1.js
+++ index1.js
@@ -2,6 +2,6 @@ const fullName = "Selfi Ardi Irawan";
 const age = 27;
 const isStudent = true;
 
-const greeting = "Hello, my name is " + fullName + ", I am " + age + " years old, and it is true that I am a student.";
-console.log(greeting);
+const greeting = `Hello, my name is ${fullName}, I am ${age} years old, and it is true that I am a student.`;
 
+console.log(greeting);
